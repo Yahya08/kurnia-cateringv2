fa8628aa0c054c8ef4ae33c8984ef05f9e0b0d60
Avivapradasyah <Pradasyahaviva>
2023-06-27 18:16:32 +0700

    page3_1

--- src/Component/Sub Component/Page3_1.jsx
+++ src/Component/Sub Component/Page3_1.jsx
@@ -4,117 +4,131 @@ function Page3_1() {
   return (
     <div className="menu-page3">
       <div className="judul-pg3">
-        <h1>Ini Judul</h1>
+        <h1 className="judulpg3-1">Hidangan Pembuka</h1>
+        <p>Hidangan pembuka adalah cara pertama untuk memulai perjalanan kuliner Anda.
+         <br /> Kami dengan bangga menyajikan sebuah hidangan yang menggoda dan memberikan sentuhan <br />
+         pertama yang tidak terlupakan
+        </p>
       </div>
       <div className="stylee-pg3">
         <div className="style-page3">
-          
           <div className="card-page3">
             <img className="img-page3" src={Img} alt="" />
             <div className="card-text3">
-              <h1 className="menu-judul-pg3">Judul</h1>
+              <h1 className="menu-judul-pg3">Risol</h1>
               <p className="deskripsi-judul-pg3">Blablabla</p>
             </div>
           </div>
 
-          <h1 className="page3-judull">judullll</h1>
+          <h1 className="page3-judull">Risol</h1>
         </div>
         <div className="style-page3">
           <div className="card-page3">
             <img className="img-page3" src={Img} alt="" />
             <div className="card-text3">
-              <h1 className="menu-judul-pg3">Judul</h1>
+              <h1 className="menu-judul-pg3">Roll Cake</h1>
               <p className="deskripsi-judul-pg3">Blablabla</p>
             </div>
           </div>
-          <h1 className="page3-judull">judullll</h1>
+          <h1 className="page3-judull">Roll Cake</h1>
         </div>
         <div className="style-page3">
           <div className="card-page3">
             <img className="img-page3" src={Img} alt="" />
             <div className="card-text3">
-              <h1 className="menu-judul-pg3">Judul</h1>
+              <h1 className="menu-judul-pg3">Dim Sum</h1>
               <p className="deskripsi-judul-pg3">Blablabla</p>
             </div>
           </div>
-          <h1 className="page3-judull">judullll</h1>
+          <h1 className="page3-judull">Dim Sum</h1>
         </div>
         
       </div>
       <div className="judul-pg3">
-        <h1>Ini Judul</h1>
+        <h1 className="judulpg3-1">Hidangan Pembuka</h1>
+        <p>Hidangan pembuka adalah cara pertama untuk memulai perjalanan kuliner Anda.
+         <br /> Kami dengan bangga menyajikan sebuah hidangan yang menggoda dan memberikan sentuhan <br />
+         pertama yang tidak terlupakan
+        </p>
       </div>
       <div className="stylee-pg3">
         <div className="style-page3">
           <div className="card-page3">
             <img className="img-page3" src={Img} alt="" />
             <div className="card-text3">
-              <h1 className="menu-judul-pg3">Judul</h1>
+              <h1 className="menu-judul-pg3">Risol</h1>
               <p className="deskripsi-judul-pg3">Blablabla</p>
             </div>
           </div>
-          <h1 className="page3-judull">judullll</h1>
+
+          <h1 className="page3-judull">Risol</h1>
         </div>
         <div className="style-page3">
           <div className="card-page3">
             <img className="img-page3" src={Img} alt="" />
             <div className="card-text3">
-              <h1 className="menu-judul-pg3">Judul</h1>
+              <h1 className="menu-judul-pg3">Roll Cake</h1>
               <p className="deskripsi-judul-pg3">Blablabla</p>
             </div>
           </div>
-          <h1 className="page3-judull">judullll</h1>
+          <h1 className="page3-judull">Roll Cake</h1>
         </div>
         <div className="style-page3">
           <div className="card-page3">
             <img className="img-page3" src={Img} alt="" />
             <div className="card-text3">
-              <h1 className="menu-judul-pg3">Judul</h1>
+              <h1 className="menu-judul-pg3">Dim Sum</h1>
               <p className="deskripsi-judul-pg3">Blablabla</p>
             </div>
           </div>
-          <h1 className="page3-judull">judullll</h1>
+          <h1 className="page3-judull">Dim Sum</h1>
         </div>
         
       </div>
       <div className="judul-pg3">
-        <h1>Ini Judul</h1>
+        <h1 className="judulpg3-1">Hidangan Pembuka</h1>
+        <p>Hidangan pembuka adalah cara pertama untuk memulai perjalanan kuliner Anda.
+         <br /> Kami dengan bangga menyajikan sebuah hidangan yang menggoda dan memberikan sentuhan <br />
+         pertama yang tidak terlupakan
+        </p>
       </div>
       <div className="stylee-pg3">
         <div className="style-page3">
           <div className="card-page3">
             <img className="img-page3" src={Img} alt="" />
             <div className="card-text3">
-              <h1 className="menu-judul-pg3">Judul</h1>
+              <h1 className="menu-judul-pg3">Risol</h1>
               <p className="deskripsi-judul-pg3">Blablabla</p>
             </div>
           </div>
-          <h1 className="page3-judull">judullll</h1>
+
+          <h1 className="page3-judull">Risol</h1>
         </div>
         <div className="style-page3">
           <div className="card-page3">
             <img className="img-page3" src={Img} alt="" />
             <div className="card-text3">
-              <h1 className="menu-judul-pg3">Judul</h1>
+              <h1 className="menu-judul-pg3">Roll Cake</h1>
               <p className="deskripsi-judul-pg3">Blablabla</p>
             </div>
           </div>
-          <h1 className="page3-judull">judullll</h1>
+          <h1 className="page3-judull">Roll Cake</h1>
         </div>
         <div className="style-page3">
           <div className="card-page3">
             <img className="img-page3" src={Img} alt="" />
             <div className="card-text3">
-              <h1 className="menu-judul-pg3">Judul</h1>
+              <h1 className="menu-judul-pg3">Dim Sum</h1>
               <p className="deskripsi-judul-pg3">Blablabla</p>
             </div>
           </div>
-          <h1 className="page3-judull">judullll</h1>
+          <h1 className="page3-judull">Dim Sum</h1>
         </div>
         
       </div>
+  
     </div>
   );
 }
 
-export default Page3_1;
+export default Page3_1;
