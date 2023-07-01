87592a2131d32c96cf2d459ebbee70f17630f6a4
Yahya08 <yahyahanda.r@students.amikom.ac.id>
2023-07-01 19:34:10 +0700

    update content

--- src/Component/Zennchi.jsx
+++ src/Component/Zennchi.jsx
@@ -1,6 +1,11 @@
 import "../Component/Component CSS/Zenn.css";
 import Img from "../assets/Img/Card/2b28d9c2-703d-4430-a653-3c971bdf0ce4.jpg";
 import Promo from "../assets/Img/Card/png1.png";
+import rendang from "../assets/Img/Card/rendang.jpeg";
+import gulai from "../assets/Img/Card/gulai-Riau.jpg";
+import mangut from "../assets/Img/Card/mangut.jpg";
+import bakso from "../assets/Img/Card/bakso.png";
+import barbaque from"../assets/Img/Card/barbecue.png";
 import { Link } from "react-router-dom";
 function Zennchi() {
   return (
@@ -14,24 +19,24 @@ function Zennchi() {
         <div className="swipe">
           
           <div className="card-makanan">
-            <img className="card-img" src={Img} alt="" />
+            <img className="card-img" src={mangut} alt="" />
             <div className="card-text">
-              <h1 className="menu-judul">Judul</h1>
-              <p className="deskripsi-judul">Blablabla</p>
+              <h1 className="menu-judul">Mangut</h1>
+              <p className="deskripsi-judul">Makanan Khas Daerah Pati</p>
             </div>
           </div>
           <div className="card-makanan">
-            <img className="card-img" src={Img} alt="" />
+            <img className="card-img" src={rendang} alt="" />
             <div className="card-text">
-              <h1 className="menu-judul">Judul</h1>
-              <p className="deskripsi-judul">Blablabla</p>
+              <h1 className="menu-judul">Rendang</h1>
+              <p className="deskripsi-judul">Makanan Khas Daerah Padang </p>
             </div>
           </div>
           <div className="card-makanan">
-            <img className="card-img" src={Img} alt="" />
+            <img className="card-img" src={gulai} alt="" />
             <div className="card-text">
-              <h1 className="menu-judul">Judul</h1>
-              <p className="deskripsi-judul">Blablabla</p>
+              <h1 className="menu-judul">Gulai Udang</h1>
+              <p className="deskripsi-judul">Makanan Khas Derah Riau </p>
             </div>
           </div>
         </div>
@@ -57,9 +62,9 @@ function Zennchi() {
                 </div>
               </div>
               <div className="deskripsipromo">
-                <h1 className="judul-anim">Judull</h1>
-                <p className="text-anim">aku juga mau</p>
-                <p className="text-anim">Hubungi kami </p>
+                <h1 className="judul-anim">AYAM GEPREK</h1>
+                <p className="text-anim">Dengan Daging Ayam Yang Pasti Halal </p>
+                <p className="text-anim1">Hubungi kami </p>
               </div>
               <img className="promo-img" src={Promo} alt="" />
             </div>
@@ -67,9 +72,9 @@ function Zennchi() {
 
             <div className="page5kanan">
               <div className="kananfoto1">
-                <img className="promokanan" src={Promo} alt="" />
+                <img className="bakso" src={bakso} alt="" />
                 <div className="kanandeskripsi">
-                  <h1 className="h1-detail">Haloo</h1>
+                  <h1 className="h1-detail">Bakso Ceker</h1>
                 </div>
                 <div className="desdeta">
                   <Link className="hub-btn" to={"/reserfasi"}>
@@ -77,18 +82,18 @@ function Zennchi() {
                   </Link>
                 
                   <div className="detail">
-                    <h1 className="h1-detail1">Nasi lemak</h1>
-                    <p className="h1-detail1">Nasilemak untuk awokawok</p>
-                    <h2 className="h1-detail1">
-                      Harga <span>20K</span>
+                    <h1 className="title">Bakso Ceker</h1>
+                    <p className="h1-detail1">Bakso Dari Daging Asli Dan Diolah Dengan Higienis</p>
+                    <h2 className="harga">
+                      Harga <span>Rp20-Rp30</span>
                     </h2>
                   </div>
                 </div>
               </div>
               <div className="kananfoto1">
-                <img className="promokanan" src={Promo} alt="" />
+                <img className="bakso" src={barbaque} alt="" />
                 <div className="kanandeskripsi">
-                  <h1 className="h1-detail">Pecel Lele</h1>
+                  <h1 className="h1-detail">Barbacue</h1>
                 </div>
                 <div className="desdeta">
                   <Link className="hub-btn" to={"/reserfasi"}>
@@ -96,10 +101,10 @@ function Zennchi() {
                   </Link>
 
                   <div className="detail">
-                    <h1 className="h1-detail1">Nasi lemak</h1>
-                    <p className="h1-detail1">Nasilemak untuk awokawok</p>
-                    <h2 className="h1-detail1">
-                      Harga <span>20K</span>
+                    <h1 className="title">Barbacue</h1>
+                    <p className="h1-detail1">Dengan Daging Pilihan & Berkualitas </p>
+                    <h2 className="harga">
+                      Harga <span>Rp40 - Rp45</span>
                     </h2>
                   </div>
                 </div>
@@ -107,7 +112,7 @@ function Zennchi() {
               <div className="kananfoto1">
                 <img className="promokanan" src={Promo} alt="" />
                 <div className="kanandeskripsi">
-                  <h1 className="h1-detail">Haloo</h1>
+                  <h1 className="h1-detail">Ayam Geprek</h1>
                 </div>
                 <div className="desdeta">
                   <Link className="hub-btn" to={"/reserfasi"}>
@@ -115,10 +120,10 @@ function Zennchi() {
                   </Link>
 
                   <div className="detail">
-                    <h1 className="h1-detail1">Nasi lemak</h1>
-                    <p className="h1-detail1">Nasilemak untuk awokawok</p>
-                    <h2 className="h1-detail1">
-                      Harga <span>20K</span>
+                    <h1 className="title">Ayam Geprek</h1>
+                    <p className="h1-detail1">Dengan Sambel Terasi Yang Pedas & Lezat </p>
+                    <h2 className="harga">
+                      Harga <span>Rp10 - Rp15</span>
                     </h2>
                   </div>
                 </div>
